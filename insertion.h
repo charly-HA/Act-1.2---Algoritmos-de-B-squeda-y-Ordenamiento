550db9820d5785d9ac7f731c966b2bea9da83e9e
charly-HA <112143707+charly-HA@users.noreply.github.com>
2022-09-06 23:28:13 -0500

    Update insertion.h

--- insertion.h
+++ insertion.h
@@ -1,4 +1,4 @@
-// =================================================================
+// =================================================================// =================================================================
 //
 // File: insertion.h
 // Author: Pedro Perez
@@ -16,6 +16,42 @@
 #include <vector>
 
 // =================================================================
+// Performs the insertion sort algorith on an array.
+//
+// @param A, an array of T elements.
+// @param size, the number of elements in the array.
+// =================================================================
+template <class T>
+int insertionSort(T *arr, int size) {
+	int co = 0;
+	for(int i = 1; i < size; i++){
+		for(int j = i; j > 0 && arr[j] < arr[j - 1]; j--){
+			co++;
+			swap(arr, j, j - 1);
+		}
+	}
+	return co;
+}
+
+// =================================================================
+// Performs the insertion sort algorith on a vector.
+//
+// @param A, a vector of T elements.
+// =================================================================
+template <class T>
+int insertionSort(std::vector<T> &v) {
+	int co = 0;
+	for(int i = 1; i < v.size(); i++){
+		for(int j = i; j > 0 && v[j] < v[j - 1]; j--){
+			co++;
+			swap(v, j, j - 1);
+		}
+	}
+	return co;
+}
+
+#endif /* INSERTION_H */
+
 // Performs the insertion sort algorith on an array.
 //
 // @param A, an array of T elements.
