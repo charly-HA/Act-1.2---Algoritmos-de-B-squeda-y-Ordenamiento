fe0d0b1954a45b2e2383ec061291e0ce624ebb7c
charly-HA <112143707+charly-HA@users.noreply.github.com>
2022-11-30 23:23:39 -0600

    Update bubble.h

--- bubble.h
+++ bubble.h
@@ -20,39 +20,42 @@
 //
 // @param A, an array of T elements.
 // @param size, the number of elements in the array.
+// @return the number of swaps.
+// @complexity O(n^2)
 // =================================================================
 template <class T>
 int bubbleSort(T *arr, int size) {
-	int co = 0;
+	int counter{};
 	for(int i = size - 1; i > 0; i--){
 		for(int j = 0; j < i; j++){
-			co++;
+			++counter;
 			if(arr[j] > arr[j + 1]){
 				swap(arr, j, j + 1);
 			}
 		}
 	}
-	return co;
+	return counter;
 }
 
 // =================================================================
 // Performs the bubble sort algorithm on an vector.
 //
 // @param v, a vector of T elements.
+// @return the number of swaps.
+// @complexity O(n^2)
 // =================================================================
 template <class T>
 int bubbleSort(std::vector<T> &v) {
-	int co = 0;
+	int counter{};
 	for(int i = v.size() - 1; i > 0; i--){
 		for(int j = 0; j < i; j++){
 			if(v[j] > v[j + 1]){
-				co++;
 				swap(v, j, j + 1);
+				++counter;
 			}
 		}
 	}
-	return co;
+	return counter;
 }
 
 #endif /* BUBBLE_H */
-
